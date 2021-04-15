e5c2a9c36f3f988f8a1268dce1825579c7e2c384
Angela Busato <busato.angela@gmail.com>
2021-04-15 11:59:11 +0200

    [add] loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 
@@ -9,5 +11,52 @@ module.exports = {
         filename: 'main.js',
     },
 
-    mode: 'development'
+    mode: 'development',
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }, {
+            test: /\.(sa|sc|c)ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "postcss-loader",
+                "sass-loader"
+            ]
+        }, {
+            test: /\.(woff|woff2|ttf|otf|eot)$/,
+            use: [{
+                loader: "file-loader",
+                options: {
+                    outputPath: 'fonts',
+                },
+            }]
+        }, {
+            test: /\.(png|jpe?g|gif|svg)$/,
+            use: [{
+                loader: "file-loader",
+                options: {
+                    outputPath: 'images'
+                }
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body'
+        }),
+        // to define the css file in one file instead of the head of the html file
+        new MiniCssExtractPlugin({
+            filename: "main.css"
+        })
+    ],
 }
