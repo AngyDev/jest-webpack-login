970bb1aecd0fef39207cde025c530f2b4b7564e2
Angela Busato <busato.angela@gmail.com>
2021-04-19 11:35:34 +0200

    [add] message error if the inputs are not valid

--- src/scripts/signIn.js
+++ src/scripts/signIn.js
@@ -3,11 +3,19 @@ const { default: Login } = require("./login");
 const signIn = document.getElementById("signIn");
 const username = document.getElementById("name");
 const password = document.getElementById("password");
+const loginError = document.getElementById("loginError");
+const errorMsg = document.getElementById("errorMsg");
+
 
 const login = new Login();
 
 signIn && signIn.addEventListener('click', (e) => {
-    e.preventDefault();
     const isValid = login.checkInput(username.value, password.value);
-    console.log(isValid);
-})
+
+    if (!isValid) {
+        loginError.style.display = "flex";
+        errorMsg.innerHTML = "Name or password is not valid";
+        e.preventDefault();
+    }
+
+});
