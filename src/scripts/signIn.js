c6c8d1904ef9c3878ca8feb36fd8079c1592d3bc
Angela Busato <busato.angela@gmail.com>
2021-04-19 14:01:53 +0200

    [add] error message function

--- src/scripts/signIn.js
+++ src/scripts/signIn.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const { default: Login } = require("./login");
 
 const signIn = document.getElementById("signIn");
@@ -6,16 +7,34 @@ const password = document.getElementById("password");
 const loginError = document.getElementById("loginError");
 const errorMsg = document.getElementById("errorMsg");
 
-
 const login = new Login();
 
+/**
+ * Displays the error and the message and doesn't reload the page
+ * @param {Event} e - The event of the button
+ * @param {String} msg - The error message
+ */
+function error(e, msg) {
+    loginError.style.display = "flex";
+    errorMsg.innerHTML = msg;
+    e.preventDefault();
+}
+
 signIn && signIn.addEventListener('click', (e) => {
-    const isValid = login.checkInput(username.value, password.value);
+    // gets the value of the input
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value;
+
+    const isValid = login.checkInput(usernameValue, passwordValue);
 
     if (!isValid) {
-        loginError.style.display = "flex";
-        errorMsg.innerHTML = "Name or password is not valid";
-        e.preventDefault();
+        error(e, "Incorrect name or password");
+    } else {
+        // Checks if the user is registered 
+        const msg = login.checkUser(usernameValue, passwordValue);
+        // The user is not registered or the password is not valid
+        if (msg != undefined) {
+            error(e, msg);
+        }
     }
-
 });
