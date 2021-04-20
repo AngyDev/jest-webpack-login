9846a0a3137d4356f0e502ad9468e6e617ba3b5a
Angela Busato <busato.angela@gmail.com>
2021-04-20 11:39:53 +0200

    [add] get data from API

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,4 +1,5 @@
 import Validation from "./validation";
+const fetch = require("node-fetch");
 
 /**
  * Class of the login form
@@ -15,7 +16,7 @@ export default class Login {
         }, {
             name: "guest",
             password: "1234"
-        }]
+        }];
     }
 
     /**
@@ -26,6 +27,50 @@ export default class Login {
     getUser(username) {
         const user = this.data.find(user => user.name === username);
         return user;
+    }
+
+    /**
+     * Gets the user
+     * @param {String} username - The username of the login user
+     * @returns Array
+     */
+    async getData(username) {
+        try {
+            // @ts-ignore
+            const response = await fetch("https://605a21feb11aba001745da26.mockapi.io/api/v1/loginUsers?name=" + username);
+
+            if (response.status != 200) {
+                console.log("Status != 200");
+                return null;
+            }
+
+            const user = await response.json();
+
+            return user;
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    }
+
+    /**
+     * Checks if the user is registered
+     * @param {String} username - The username from the login
+     * @param {String} password - The password from the login
+     * @returns String - The error message
+     */
+    async checkData(username, password) {
+        const user = await this.getData(username);
+
+        // the user is not present
+        if (user.length == 0) {
+            return "The user is not registered";
+        } else {
+            // the user is present
+            if (user[0].password !== password) {
+                return "The password is wrong!";
+            }
+        }
     }
 
     /**
@@ -46,8 +91,6 @@ export default class Login {
                 return "The password is wrong!";
             }
         }
-
-        return;
     }
 
     /**
