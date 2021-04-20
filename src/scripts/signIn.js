59b9d43af7cd14c9d6e63ca57a7ef28bfedf2c19
Angela Busato <busato.angela@gmail.com>
2021-04-20 19:00:59 +0200

    [fix] signIn button to create the userLogin function

--- src/scripts/signIn.js
+++ src/scripts/signIn.js
@@ -10,17 +10,18 @@ const login = new Login();
 
 /**
  * Displays the error and the message and doesn't reload the page
- * @param {Event} e - The event of the button
  * @param {String} msg - The error message
  */
-function error(e, msg) {
+export function error(msg) {
     loginError.style.display = "flex";
     errorMsg.innerHTML = msg;
-    e.preventDefault();
 }
 
-signIn && signIn.addEventListener('click', async(e) => {
-    e.preventDefault();
+/**
+ * Checks if the inputs are valid and if the user is present
+ */
+export async function userLogin() {
+
     // gets the value of the input
     const usernameValue = username.value.trim();
     const passwordValue = password.value;
@@ -28,7 +29,7 @@ signIn && signIn.addEventListener('click', async(e) => {
     const isValid = login.checkInput(usernameValue, passwordValue);
 
     if (!isValid) {
-        error(e, "Incorrect name or password");
+        error("Incorrect name or password");
     } else {
         // Checks if the user is registered 
         try {
@@ -36,7 +37,8 @@ signIn && signIn.addEventListener('click', async(e) => {
 
             // The user is not registered or the password is not valid
             if (msg != undefined) {
-                error(e, msg);
+                error(msg);
+                //return msg;
             } else {
                 window.location.assign("https://www.google.com");
             }
@@ -45,4 +47,10 @@ signIn && signIn.addEventListener('click', async(e) => {
             console.log(error);
         }
     }
+
+}
+
+signIn && signIn.addEventListener('click', (e) => {
+    e.preventDefault();
+    userLogin();
 });
